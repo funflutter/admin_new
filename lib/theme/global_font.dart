9748b61c78049df0f07bb0e40d484cd64be9e0a3
funflutter <funwithflutter11@gmail.com>
2025-06-04 16:42:11 +0530

    global font

--- lib/theme/global_font.dart
+++ lib/theme/global_font.dart
@@ -2,26 +2,202 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppFonts {
-  static TextStyle Regular24 = GoogleFonts.nunito(
+  // font - 32
+  static TextStyle Regular32 = GoogleFonts.manrope(
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle Medium32 = GoogleFonts.manrope(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle semibold32 = GoogleFonts.manrope(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle bold32 = GoogleFonts.manrope(
+    fontSize: 32,
+    fontWeight: FontWeight.w800,
+    letterSpacing: -0.3,
+  );
+
+  // font - 28
+  static TextStyle Regular28 = GoogleFonts.manrope(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle Medium28 = GoogleFonts.manrope(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle semibold28 = GoogleFonts.manrope(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle bold28 = GoogleFonts.manrope(
+    fontSize: 28,
+    fontWeight: FontWeight.w800,
+    letterSpacing: -0.3,
+  );
+
+  // font - 24
+  static TextStyle Regular24 = GoogleFonts.manrope(
     fontSize: 24,
     fontWeight: FontWeight.w500,
     letterSpacing: -0.3,
   );
 
-  static TextStyle Medium24 = GoogleFonts.mulish(
+  static TextStyle Medium24 = GoogleFonts.manrope(
     fontSize: 24,
     fontWeight: FontWeight.w600,
     letterSpacing: -0.3,
   );
 
-  static TextStyle semibold24 = GoogleFonts.mulish(
+  static TextStyle semibold24 = GoogleFonts.manrope(
     fontSize: 24,
     fontWeight: FontWeight.w700,
     letterSpacing: -0.3,
   );
 
-  static TextStyle bold24 = GoogleFonts.mulish(
+  static TextStyle bold24 = GoogleFonts.manrope(
     fontSize: 24,
+    fontWeight: FontWeight.w800,
+    letterSpacing: -0.3,
+  );
+
+  // font - 20
+  static TextStyle Regular20 = GoogleFonts.manrope(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle Medium20 = GoogleFonts.manrope(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle semibold20 = GoogleFonts.manrope(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle bold20 = GoogleFonts.manrope(
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
+    letterSpacing: -0.3,
+  );
+
+  // font - 18
+  static TextStyle Regular18 = GoogleFonts.manrope(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle Medium18 = GoogleFonts.manrope(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle semibold18 = GoogleFonts.manrope(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle bold18 = GoogleFonts.manrope(
+    fontSize: 18,
+    fontWeight: FontWeight.w800,
+    letterSpacing: -0.3,
+  );
+
+  // font - 16
+  static TextStyle Regular16 = GoogleFonts.manrope(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle Medium16 = GoogleFonts.manrope(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle semibold16 = GoogleFonts.manrope(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle bold16 = GoogleFonts.manrope(
+    fontSize: 16,
+    fontWeight: FontWeight.w800,
+    letterSpacing: -0.3,
+  );
+
+  // font - 14
+  static TextStyle Regular14 = GoogleFonts.manrope(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle Medium14 = GoogleFonts.manrope(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle semibold14 = GoogleFonts.manrope(
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle bold14 = GoogleFonts.manrope(
+    fontSize: 14,
+    fontWeight: FontWeight.w800,
+    letterSpacing: -0.3,
+  );
+
+  // font - 12
+  static TextStyle Regular12 = GoogleFonts.manrope(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle Medium12 = GoogleFonts.manrope(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle semibold12 = GoogleFonts.manrope(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.3,
+  );
+
+  static TextStyle bold12 = GoogleFonts.manrope(
+    fontSize: 12,
     fontWeight: FontWeight.w800,
     letterSpacing: -0.3,
   );
