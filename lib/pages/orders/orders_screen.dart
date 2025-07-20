2e06f0364ff5b0ffa322cab6406183b703279e4f
funflutter <funwithflutter11@gmail.com>
2025-07-20 23:19:33 +0530

    order view navigation

--- lib/pages/orders/orders_screen.dart
+++ lib/pages/orders/orders_screen.dart
@@ -1,5 +1,6 @@
 import 'package:admin_ecom/components/custom_app_bar.dart';
 import 'package:admin_ecom/constant.dart';
+import 'package:admin_ecom/pages/orders/order_view.dart';
 import 'package:admin_ecom/theme/global_font.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -86,108 +87,117 @@ class _OrdersScreenState extends State<OrdersScreen> {
                             return Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Row(
-                                  children: [
-                                    // Image Box
-                                    Container(
-                                      height: 80,
-                                      width: 90,
-                                      decoration: BoxDecoration(
-                                        color: Color(0xffE6E6E6),
-                                        borderRadius: BorderRadius.circular(6),
+                                GestureDetector(
+                                  onTap: () => Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => OrderView(),
+                                      )),
+                                  child: Row(
+                                    children: [
+                                      // Image Box
+                                      Container(
+                                        height: 80,
+                                        width: 90,
+                                        decoration: BoxDecoration(
+                                          color: Color(0xffE6E6E6),
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                        ),
                                       ),
-                                    ),
-                                    SizedBox(width: 10),
+                                      SizedBox(width: 10),
 
-                                    // Product info + right side number
-                                    Expanded(
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          // Left Column
-                                          Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                'Bracelet (Gold)',
-                                                style: AppFonts.semibold16(
-                                                  Colors.black,
-                                                ),
-                                              ),
-                                              SizedBox(height: 2),
-                                              Row(
-                                                children: [
-                                                  Text(
-                                                    'Qty:',
-                                                    style: AppFonts.Medium14(
-                                                      Color(0xff5E625E),
-                                                    ),
+                                      // Product info + right side number
+                                      Expanded(
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            // Left Column
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  'Bracelet (Gold)',
+                                                  style: AppFonts.semibold16(
+                                                    Colors.black,
                                                   ),
-                                                  SizedBox(width: 2),
-                                                  Text(
-                                                    '02',
-                                                    style: AppFonts.Medium14(
-                                                      Colors.black,
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-
-                                              SizedBox(height: 16),
-
-                                              // discount container
-                                              Container(
-                                                height: 22,
-                                                decoration: BoxDecoration(
-                                                  color: Color(0xffEAF6EC),
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          100),
                                                 ),
-                                                child: Padding(
-                                                  padding: const EdgeInsets
-                                                      .symmetric(
-                                                      horizontal: 8.0),
-                                                  child: Row(
-                                                    children: [
-                                                      Text(
-                                                        'Delivered',
-                                                        style:
-                                                            AppFonts.Medium14(
-                                                          Color(0xff28A745),
-                                                        ),
+                                                SizedBox(height: 2),
+                                                Row(
+                                                  children: [
+                                                    Text(
+                                                      'Qty:',
+                                                      style: AppFonts.Medium14(
+                                                        Color(0xff5E625E),
                                                       ),
-                                                    ],
+                                                    ),
+                                                    SizedBox(width: 2),
+                                                    Text(
+                                                      '02',
+                                                      style: AppFonts.Medium14(
+                                                        Colors.black,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+
+                                                SizedBox(height: 16),
+
+                                                // discount container
+                                                Container(
+                                                  height: 22,
+                                                  decoration: BoxDecoration(
+                                                    color: Color(0xffEAF6EC),
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            100),
+                                                  ),
+                                                  child: Padding(
+                                                    padding: const EdgeInsets
+                                                        .symmetric(
+                                                        horizontal: 8.0),
+                                                    child: Row(
+                                                      children: [
+                                                        Text(
+                                                          'Delivered',
+                                                          style:
+                                                              AppFonts.Medium14(
+                                                            Color(0xff28A745),
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
                                                   ),
                                                 ),
-                                              ),
-                                            ],
-                                          ),
+                                              ],
+                                            ),
 
-                                          // Quantity Circle
-                                          Container(
-                                            height: 20,
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(50),
-                                              color: Colors.black,
+                                            // Quantity Circle
+                                            Container(
+                                              height: 20,
+                                              decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(50),
+                                                color: Colors.black,
+                                              ),
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 6.0),
+                                              child: Center(
+                                                child: Text('12',
+                                                    style: AppFonts.Medium12(
+                                                        Colors.white)),
+                                              ),
                                             ),
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 6.0),
-                                            child: Center(
-                                              child: Text('12',
-                                                  style: AppFonts.Medium12(
-                                                      Colors.white)),
-                                            ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                                 if (index != 2) ...[
                                   SizedBox(height: 10),
